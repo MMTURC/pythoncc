7948c15e99d84814ee5460ed45fc6a2fbc4caa57
mauricio <111543689+MMTURC@users.noreply.github.com>
2026-01-15 18:55:25 -0500

    Numbers

---
+++ numbers/numbers.py
@@ -0,0 +1,16 @@
+# Write addition, subtraction, multiplication and division operations that each result in number 8
+
+
+addition = 4 + 4
+
+subtraction = 10 - 2 
+
+multiplication = 4 * 2
+
+division = 16 / 2
+
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
